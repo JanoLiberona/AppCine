5bfbe16023c4b8eb89c857b5468ae14109349f4a
JavierSuarezGuzman <ja.suarezg496@gmail.com>
2021-10-15 23:45:23 -0300

    mail a dashboard

--- app/src/main/java/com/example/appcine/DashboardActivity.java
+++ app/src/main/java/com/example/appcine/DashboardActivity.java
@@ -3,12 +3,23 @@ package com.example.appcine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    TextView txtNameUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        //OBTENEMOS VARIABLE DE NUETRA ACTIVIDAD ANTERIOR
+        String nameUser = getIntent().getStringExtra("nameUser");
+        //ASIGNAREMOS DICHA VARIABLE A EL TextView generado
+        txtNameUser = (TextView) findViewById(R.id.nameUser);
+        txtNameUser.setText(nameUser);
+
+
     }
 }
--- app/src/main/java/com/example/appcine/MainActivity.java
+++ app/src/main/java/com/example/appcine/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.appcine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextInputLayout tilUser;
     private Button btnLogin, btnRegister;
+
+    //SHARED PREFERENCES
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor sharedEditor;
 
 
     @Override
@@ -17,13 +27,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tilUser = (TextInputLayout) findViewById(R.id.til_user);
+
         btnLogin = findViewById(R.id.btn_login);
         btnRegister = findViewById(R.id.btn_register);
+
+        //INICIALIZAMOS LAS VARIABLES SHARED
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedEditor = sharedPreferences.edit();
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), RegisterActivity.class);
+
+
+
                 startActivity(intent);
             }
         });
@@ -32,6 +51,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), DashboardActivity.class);
+
+                String mailUser = tilUser.getEditText().getText().toString();
+                mailUser = mailUser.split("@")[0];
+                intent.putExtra("nameUser",mailUser);
+                
                 startActivity(intent);
             }
         });
